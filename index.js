cb3cb67819096d37c623de8d52c74f2c6f57dca6
WawanC <kurniawancristianto@gmail.com>
2022-06-14 19:55:44 +0700

    nambah feature lagi

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use("/product", productRoutes);
 
 console.log("Feature B");
+console.log('Feature B2')
 
 app.listen(8000, async () => {
   console.log("Listening on 8000");
